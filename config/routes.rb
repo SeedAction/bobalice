b4e9c2c1624a6a5ff988f1ca36c70df5e358b7ed
TazMayhem <anselin.lohner@epsi.fr>
2016-04-02 15:43:33 +0200

    Adding the Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root 'application#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
