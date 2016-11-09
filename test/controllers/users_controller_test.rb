95fad62d5829ea13e0fc70684586c822565ee210
SeedAction <anselin.lohner@gmail.com>
2016-11-09 17:05:38 +0100

    Completes the layout and routes

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Bobalice"
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+    assert_select "title", "Sign up | #{@base_title}"
+  end
+end
